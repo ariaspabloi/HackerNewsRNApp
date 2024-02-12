e4b8ac295e76dc7cb8126ec22cbef2255cf8a2b3
Pablo Arias <pabloarias.ic@gmail.com>
2024-02-12 14:54:38 -0300

    feat: add and configure GestureHandler package

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {StackNavigator} from './app/presentation/navigation/stackNavigator';
 
 function App(): React.JSX.Element {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      <GestureHandlerRootView style={{flex: 1}}>
+        <StackNavigator />
+      </GestureHandlerRootView>
     </NavigationContainer>
   );
 }
